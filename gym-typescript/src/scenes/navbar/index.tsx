c92fc2ad5a6894540535568285c794032b87d4ea
Salavat <salavat2602@mail.ru>
2025-09-18 11:49:05 +0500

    any

--- gym-typescript/src/scenes/navbar/index.tsx
+++ gym-typescript/src/scenes/navbar/index.tsx
@@ -1,41 +1,42 @@
-import { useState} from "react";
+import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
-import Logo from "../../assets/Logo.png"
+import Logo from "../../assets/Logo.png";
+import { select } from "framer-motion/client";
+import Link from "./Link";
+import type { SelectedPage } from "@/shared/types";
 
+type Props = {
+     selectedPage: SelectedPage;
+    setSelectedPage: (value: SelectedPage) => void;
+};
 
-type Props = {}
-
-const Navbar = (props: Props) => {
-    const flexBeetwen = "flex items-center justify-between"
+const Navbar = ({selectedPage, setSelectedPage}: Props) => {
+  const flexBeetwen = "flex items-center justify-between";
   return (
-    <nav >
-        <div
-        className={`${flexBeetwen} fixed top-0 z-30 w-full py-6 bg-white`}
-        >
-            <div className={`${flexBeetwen} mx-auto w-5/6`}>
-                <div className={`${flexBeetwen} w-full gap-16`}>
-                    <img src={Logo} alt="logo" />
-                    <div className={`${flexBeetwen} w-full`}>
-                    <div className={`${flexBeetwen} gap-8 text-sm`}>
-                        <p>Home</p>
-                        <p>Benefits</p>
-                        <p>Our Classes</p>
-                        <p>Contact Us</p>
-
-                    </div>
-                    <div className={`${flexBeetwen} gap-8`}>
-                        <p>Sign In</p>
-                        <button>Become a member</button>
-                    </div>
-
-                    </div>
-
-                </div>
+    <nav>
+      <div className={`${flexBeetwen} fixed top-0 z-30 w-full py-6 bg-white`}>
+        <div className={`${flexBeetwen} mx-auto w-5/6`}>
+          <div className={`${flexBeetwen} w-full gap-16`}>
+            <img src={Logo} alt="logo" />
+            <div className={`${flexBeetwen} w-full`}>
+              <div className={`${flexBeetwen} gap-8 text-sm`}>
+                <Link page="Home" selectedPage={selectedPage} />
+                <Link page="Benefits" selectedPage={selectedPage} />
+                <Link page="Our Classes" selectedPage={selectedPage} />
+                <Link page="Contact Us" selectedPage={selectedPage}/>
+              </div>
+              <div className={`${flexBeetwen} gap-8`}>
+                <p>Sign In</p>
+                <button className="rounded-full bg-secondary-500 py-2 px-5 cursor-pointer ">
+                  Become a member
+                </button>
+              </div>
             </div>
-
+          </div>
         </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
